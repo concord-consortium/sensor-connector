3d754227c3d15a1e4593e332abdf7709057027fe
Aaron Unger <aaron.unger@gmail.com>
2014-03-17 14:18:17 -0700

    Change port and host to 11180 on localhost

--- src/main/java/org/concord/sensor/server/SensorServer.java
+++ src/main/java/org/concord/sensor/server/SensorServer.java
@@ -1,5 +1,8 @@
 package org.concord.sensor.server;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 import org.eclipse.jetty.server.Server;
 
 /**
@@ -10,7 +13,9 @@ public class SensorServer
 {
     public static void main( String[] args ) throws Exception
     {
-    	Server server = new Server(8088);
+    	// 11180 seems unassigned, high enough to not be privileged
+    	// Attach only to localhost (for now), to avoid any firewall popups
+    	Server server = new Server(new InetSocketAddress(InetAddress.getByName(null), 11180));
         server.setHandler(new SensorHandler());
         server.start();
         server.join();
