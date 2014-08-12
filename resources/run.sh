77eb3d6cc8203108a6075c83cc24bb24718b90b6
Aaron Unger <aaron.unger@gmail.com>
2014-08-12 09:46:26 -0700

    Ignore globally installed extensions

--- resources/run.sh
+++ resources/run.sh
@@ -5,4 +5,4 @@ cd ..
 export JAVA_HOME=$(pwd -P)/Resources/jdk
 export PATH=$JAVA_HOME/Home/bin:$PATH
 
-java -d32 -Dapple.awt.UIElement=true -Xdock:icon=Resources/cc-lightbulb.icns -jar Java/sensor-connector-0.0.1-SNAPSHOT.jar
+java -d32 -Djava.ext.dir="" -Dapple.awt.UIElement=true -Xdock:icon=Resources/cc-lightbulb.icns -jar Java/sensor-connector-0.0.1-SNAPSHOT.jar
