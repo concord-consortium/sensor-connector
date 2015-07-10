759a2edf1475889044abb1acc604e61a7020e1a6
Aaron Unger <aunger@concord.org>
2015-07-10 12:45:14 -0700

    Remove plugin; install ca cert

--- script/create_pkg.sh
+++ script/create_pkg.sh
@@ -2,7 +2,6 @@
 
 # NOTE: This is all probably really brittle!
 
-cp -r resources/npSensorConnectorDetection.plugin dist/package
 mv dist/app/SensorConnector.app dist/package/
 
 # Requires PackageMaker to be installed in /Applications
@@ -13,7 +12,6 @@ cd dist/package
 # TODO Using pkgbuild and productbuild is more future-proof, but I ran out of time
 # cd dist/package
 # pkgbuild --identifier org.concord.sensorconnector.app.pkg --version 1.0.0.20140814 --root SensorConnector.app --install-location /Applications/SensorConnector.app SC.pkg
-# pkgbuild --identifier org.concord.sensorconnector.plugin.pkg --version 1.0.0.20140814 --component npSensorConnectorDetection.plugin --install-location /Library/Internet\ Plug-Ins/ plugin.pkg
 # productbuild --distribution SC.xml --package-path . SC-unsigned.pkg
 # productsign --sign "Developer ID Installer: Concord Consortium Inc (T8HS8WBPPQ)" SC-unsigned.pkg ../app/SensorConnector.pkg
 
