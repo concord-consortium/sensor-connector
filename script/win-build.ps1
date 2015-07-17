ed353cd7688016dbc786c2b8e72fe38be790b742
Aaron Unger <aunger@concord.org>
2015-07-17 15:31:41 -0700

    Fix win builds

--- script/win-build.ps1
+++ script/win-build.ps1
@@ -1,7 +1,12 @@
-#!/bin/sh
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+cd $dir
+cd ..
 
 rm dist -r -Force
 ant win-bundle
 mv dist/SensorConnector-*.msi .
 mv dist/SensorConnector-*.exe .
 rm *unsigned*
+
+# Read-Host -Prompt "Press Enter to exit"
