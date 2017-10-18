e45ed46edfa4bf0725bc3e66ff6cc669323fa104
Kirk Swenson <kswenson@concord.org>
2017-10-18 14:01:53 -0700

    First attempt to update the build scripts and instructions - eliminate dependence on `https://source.concord.org` Concord-hosted Maven repository - eliminate hard-coded JDK version name - embed `Tungsten-FSM` locally - update instructions to include local installation of JARs - update instructions to include Java 8 build status

--- script/mac-build.sh
+++ script/mac-build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-rm -rf dist
-ant mac-package-x64 && mv dist/SensorConnector-x64-*.dmg .
+export JAVA_HOME=`/usr/libexec/java_home`
 
+# build 32-bit package
 rm -rf dist
+export JRE_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK
 ant mac-package && mv dist/SensorConnector-*.dmg .
+
+# build 64-bit package
+rm -rf dist
+export JRE_HOME=$JAVA_HOME
+ant mac-package-x64 && mv dist/SensorConnector-x64-*.dmg .
