2811db71397d5fed1d9d60e4ee6c2de8572ae9e1
Kirk Swenson <kswenson@concord.org>
2018-03-12 12:00:44 -0700

    Update `tungsten-fsm` library

--- script/install-local.sh
+++ script/install-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn install:install-file -Dfile=lib/tungsten-fsm.jar -DgroupId=com.continuent.tungsten -DartifactId=tungsten-fsm -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/tungsten-fsm.jar -DgroupId=com.continuent.tungsten -DartifactId=tungsten-fsm -Dversion=1.2 -Dpackaging=jar
