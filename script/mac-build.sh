8d5e8ff1043b739c48eab4a6715763b7b1aa8bfb
Kirk Swenson <kswenson@concord.org>
2018-08-09 14:24:15 -0700

    Fix 32-bit Mac OS build on High Sierra (10.13) [#159673925]

--- script/mac-build.sh
+++ script/mac-build.sh
@@ -8,7 +8,9 @@ export JAVA_HOME=`/usr/libexec/java_home`
 # build 32-bit package
 if [[ ("$1" == "-32") || ($# -eq 0) ]]; then
   rm -rf dist
-  export JRE_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK
+  JRE32_HOME=`/usr/libexec/java_home -d 32`
+  # for Java6 we want the parent directory
+  export JRE_HOME="$(dirname "$JRE32_HOME")"
   ant mac-package && mv dist/SensorConnector-*.dmg .
 fi
 
