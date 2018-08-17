475890580e65fa8163217847ef5f502be306c207
Kirk Swenson <kswenson@concord.org>
2018-08-17 07:39:52 -0700

    Update Mac to set/use SC_APP_VERSION and SC_ARCH_BITS environment variables

--- script/mac-build.sh
+++ script/mac-build.sh
@@ -4,6 +4,7 @@
 #        Use "-32" or "-64" argument to build 32-bit/64-bit package only
 
 export JAVA_HOME=`/usr/libexec/java_home`
+export SC_APP_VERSION=`cat version.txt`
 
 # build 32-bit package
 if [[ ("$1" == "-32") || ($# -eq 0) ]]; then
@@ -11,6 +12,7 @@ if [[ ("$1" == "-32") || ($# -eq 0) ]]; then
   JRE32_HOME=`/usr/libexec/java_home -d 32`
   # for Java6 we want the parent directory
   export JRE_HOME="$(dirname "$JRE32_HOME")"
+  export SC_ARCH_BITS=32
   ant mac-package && mv dist/SensorConnector-*32.dmg .
 fi
 
@@ -18,5 +20,6 @@ fi
 if [[ ("$1" == "-64") || ($# -eq 0) ]]; then
   rm -rf dist
   export JRE_HOME=$JAVA_HOME
+  export SC_ARCH_BITS=64
   ant mac-package-x64 && mv dist/SensorConnector-*64.dmg .
 fi
